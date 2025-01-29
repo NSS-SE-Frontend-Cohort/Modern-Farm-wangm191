5d7d1acb560de9244d4b0d0f7777d24321c8daa1
Myles Wang <formyleswang@gmail.com>
2025-01-29 18:54:42 -0500

    updated functions with toLowerCase(), added sorting function for harvested plants and included emoji with html string to be displayed in browser

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -1,14 +1,15 @@
 export const harvestPlants = (plantsArray) => {
     const harvestArray = []
+    let currentId = 0;
     plantsArray.forEach(plant => {
-        if (plant.type !== "Corn"){
+        if (plant.type.toLowerCase() !== "corn"){
             for ( let i = 0; i < plant.output; i++ ) {
-                harvestArray.push(plant)
+                harvestArray.push({ ...plant, id: currentId++ });
             }
         }
-        else if (plant.type === "Corn") {
+        else if (plant.type.toLowerCase() === "corn") {
             for ( let i = 0; i < plant.output / 2; i++ ){
-                harvestArray.push(plant)
+                harvestArray.push({ ...plant, id: currentId++ });
             }
         }
     });
