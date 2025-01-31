0e0608576fb23dc6652d95663cd9d9f8aba2ed23
Myles Wang <formyleswang@gmail.com>
2025-01-31 14:39:40 -0500

    created processor() within processorFacility.js, updated main.js to include queue datastructure and methods

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 import { catalog, renderCatalog } from "./catalog.js"
 import { harvestPlants } from "./harvester.js"
 import { createPlan } from "./plan.js"
+import { processor } from "./processingFacility.js"
 import { barn } from "./storageBarn.js"
 import { plantSeeds } from "./tractor.js"
 
@@ -27,6 +28,19 @@ if (!storageBarn.isEmpty()) {
 }
 console.log(storageBarn.pop())
 console.log(storageBarn.getStorage())
+
+const processorFacility = processor()
+
+console.log(processorFacility.isEmpty())
+console.log(processorFacility.enqueue(harvestedArray[0]))
+console.log(processorFacility.enqueue(harvestedArray[1]))
+
+console.log(processorFacility.next())
+console.log(processorFacility.last())
+console.log(processorFacility.size())
+
+console.log(processorFacility.dequeue())
+console.log(processorFacility.isEmpty())
 
 const catalogHtml = catalog(harvestedArray)
 renderCatalog(catalogHtml)
